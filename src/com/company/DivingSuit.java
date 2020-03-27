20ba87c9866ee78089357c17f86a0ef7a81e2b70
Parziwal <22746260+Parziwal@users.noreply.github.com>
2020-03-27 13:34:26 +0100

    Add Pickables

---
+++ src/com/company/DivingSuit.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * A buvárruhát reprezentáló osztály. Ha a játékoson van búvárruha, akkor vízbe esés esetén ki tud mászni a vízből.
+ */
+public class DivingSuit implements Pickable {
+    /**
+     * A paraméterben kapott karakteren meghívja makeDiver() függvényt, ezzel a játékos felveszi a búvárruhát.
+     * @param c - A karakter, aki felveszi a tárgyat.
+     */
+    public void pickUp(Character c) {
+
+    }
+}
---
+++ src/com/company/SignalRocketParts.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * A jelzőrakéta összeszereléséhez kellő alkatrész. A játék megnyeréséhez szükséges a megszerzésük. Összesen 3 ilyen van.
+ */
+public class SignalRocketParts implements Pickable {
+    /**
+     * A paraméterben kapott karakteren meghívja a BuildSignalRocket(Pickable: i) függvényt, ezzel hozzáadva magát a SignalRocket-hez.
+     * @param c - A karakter, aki felveszi a tárgyat.
+     */
+    public void pickUp(Character c) {
+
+    }
+}
