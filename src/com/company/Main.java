4b4537cd6437a2b8973c2b904ca87c3580f51fb4
Parziwal <22746260+Parziwal@users.noreply.github.com>
2020-03-28 13:15:52 +0100

    Menu for initialization

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,21 @@
 package com.company;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     static public String tab = "";
 
     public static void main(String[] args) {
-        Stable iceTable = new Stable(null);
-        Eskimo e = new Eskimo(null ,iceTable);
-        iceTable.stepOn(e);
-        e.useAbility(iceTable);
+        Scanner in = new Scanner(System.in);
+        Menu menu = new Menu();
+        int menuItem = -1;
+        menu.writeOutMenu();
+        while(menuItem != 0) {
+            System.out.print("Menüpont száma: ");
+            menuItem = in.nextInt();
+            menu.chooseMenuItem(menuItem);
+        }
+        in.close();
     }
 }
